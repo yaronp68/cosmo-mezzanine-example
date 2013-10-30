e8ed579afc1f64df9aa0f61d70342a0d1ba29779
Idan Moyal <idan@gigaspaces.com>
2013-10-30 16:13:25 +0200

    added install_logstash to management bootstrap call

--- cosmo-install-manager.sh
+++ cosmo-install-manager.sh
@@ -5,7 +5,6 @@ sudo apt-get -y update && sudo apt-get install -y python-dev git rsync openjdk-7
 sudo update-alternatives --set java /usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java
 
 # configure and clone cosmo-manager from github
-DEV_MODE=
 BRANCH=develop
 WORKINGDIR=$HOME/cosmo-work
 # VERSION=0.1-RELEASE
@@ -17,14 +16,12 @@ git clone https://github.com/CloudifySource/cosmo-manager.git
 ( cd cosmo-manager ; git checkout $BRANCH )
 
 # bootstrap this machine as a management machine
-python2.7 cosmo-manager/vagrant/bootstrap_lxc_manager.py --working_dir=$WORKINGDIR --cosmo_version=$VERSION --config_dir=$CONFIGDIR --install_openstack_provisioner=True
+python2.7 cosmo-manager/vagrant/bootstrap_lxc_manager.py --working_dir=$WORKINGDIR --cosmo_version=$VERSION --config_dir=$CONFIGDIR --install_openstack_provisioner --install_logstash
 
 if [ $? -ne 0 ]; then
     exit $?
 fi
 
-if [ "$DEV_MODE" ]; then
-    mvn clean package -DskipTests -Pall -f cosmo-manager/orchestrator/pom.xml
-    rm cosmo.jar
-    cp cosmo-manager/orchestrator/target/cosmo.jar .
-fi
+mvn clean package -DskipTests -Pall -f cosmo-manager/orchestrator/pom.xml
+rm cosmo.jar
+cp cosmo-manager/orchestrator/target/cosmo.jar .
